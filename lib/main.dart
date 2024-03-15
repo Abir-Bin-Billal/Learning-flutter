6fabd25b078ad90df5ea2b3120f9c5f3f21ac74d
Abir <abir41300@gmail.com>
2024-03-15 14:49:02 +0600

    pageView added

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:my_calculator/HomeApp.dart';
-import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:my_calculator/page1.dart';
+import 'package:my_calculator/page2.dart';
+import 'package:my_calculator/page3.dart';
+import 'package:my_calculator/page4.dart';
 
-void main() {
-  runApp(Myapp());
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+   MyApp({super.key});
+ 
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class Myapp extends StatelessWidget{
+class _MyAppState extends State<MyApp> {
+   final PageController _controller = PageController(
+    initialPage: 0,
+    
+  );
+
   @override
+  void dispose() {
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeApp(),
+      home: Scaffold(
+        body: PageView(
+          controller: _controller,
+          scrollDirection: Axis.vertical,
+          children: const [
+            page1(),
+            page2(),
+            page3(),
+            page4(),
+          ],
+        ),
+      ),
     );
   }
 }
