cb72c651154ec3ef35f374f02627f2a5cb13a78a
Abir <abir41300@gmail.com>
2024-03-18 02:55:20 +0600

    showModalButtomSheet added

--- lib/main.dart
+++ lib/main.dart
@@ -7,72 +7,51 @@ void main(){
   runApp(Myapp());
 }
 
-class Myapp extends StatefulWidget {
+class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
-  State<Myapp> createState() => _MyappState();
-}
-
-class _MyappState extends State<Myapp> {
-  @override
-
-
-File? _image;
-Future getImage() async{
-  final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-  if (image == null) {
-    return;
-  }
-  final ImageTemporary = File(image.path);
-  setState(() {
-    this._image = ImageTemporary;
-  });
-}
-Future getCamera() async{
-  final image = await ImagePicker().pickImage(source: ImageSource.camera);
-  if (image == null) {
-    return;
-  }
-  final ImageTemporary = File(image.path);
-  setState(() {
-    this._image = ImageTemporary;
-  });
-}
-
-
-
-
   Widget build(BuildContext context) {
-    
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: 500,
-              width: double.infinity,
-              color: Colors.blue,
-              child: _image==null?Text("no imaees"):Image.network(_image!.path , fit: BoxFit.cover,)
-            
+     return MaterialApp(debugShowCheckedModeBanner: false,
+     home:  home());
+     
+  }
+}
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: ElevatedButton(onPressed: (){showModalBottomSheet<void>(context: context, builder: (BuildContext context){
+            return Container(
+              height: 250,
               
-            ),
-            Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              
-              children: [
-                FloatingActionButton(onPressed: (){getCamera();}, child: Icon(Icons.camera),),
-                FloatingActionButton(onPressed: (){getImage();} , child: Icon(Icons.browse_gallery),)
-              ],
-            )
-          ],
-        ),
-    
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text("Abir"),
+                    subtitle: Text("The litti-boy"),
+                    leading: Icon(Icons.home),
+                  ),ListTile(
+                    title: Text("Jarif"),
+                    subtitle: Text("The starboy"),
+                    leading: Icon(Icons.home),
+                  ),ListTile(
+                    title: Text("aadeeb"),
+                    subtitle: Text("The bundasting"),
+                    leading: Icon(Icons.home),
+                  ),
+                  ElevatedButton(onPressed: (){Navigator.pop(context);}, child: Text("Back"))
+                ],
+              ),
+            );
+
+
+          });} , child: Text("Click me!"),),
       ),
     );
-    
   }
-
 }
