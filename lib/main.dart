4d21bdb77dfe9e6dfac8155b4c08e420e54699fc
Abir <abir41300@gmail.com>
2024-03-25 19:40:10 +0600

    Snackbar added

--- lib/main.dart
+++ lib/main.dart
@@ -18,89 +18,24 @@ class Myapp extends StatelessWidget {
 
 class Home extends StatefulWidget {
   const Home({super.key});
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
   @override
+  MySnackbar(String message ,BuildContext context){
+  return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(message) , 
+    duration: Duration(seconds: 5),
+    action: SnackBarAction(label: "Ok", onPressed: (){}),
+    
+    ));
+}
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return Dialog(
-                      child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Stack(
-                            alignment: Alignment.topCenter,
-                            clipBehavior: Clip.none,
-                            children: [
-                              Container(
-                                height: 250,
-                                width: 250,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(height: 10),
-                                    Text(
-                                      "Alert Dialog",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5,
-                                    ),
-                                    Text(
-                                      "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.",
-                                      style: TextStyle(fontSize: 15),
-                                    ),
-                                    SizedBox(height: 10),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: Text("No"),
-                                          style: ElevatedButton.styleFrom(
-                                              backgroundColor: Colors.red),
-                                        ),
-                                        SizedBox(
-                                          width: 7,
-                                        ),
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: Text("Yes"),
-                                          style: ElevatedButton.styleFrom(
-                                              backgroundColor: Colors.blue),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Positioned(
-                                  top: -70,
-                                  child: CircleAvatar(
-                                      radius: 50,
-                                      child: Image.network(
-                                          "https://cdn.pixabay.com/photo/2017/07/08/18/54/decor-2485268_640.png")))
-                            ],
-                          )),
-                    );
-                  });
-            },
-            child: Text("Click me!")),
+        child: ElevatedButton(onPressed: (){MySnackbar("Are you sure?", context);}, child: Text('Click me')),
       ),
     );
   }
