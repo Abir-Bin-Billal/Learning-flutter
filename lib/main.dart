22436f5c1a9b144a6f420506b36c31e9d92adceb
Abir <abir41300@gmail.com>
2024-04-04 20:04:27 +0600

    Checkbox added

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,52 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:http/http.dart' as http;
 
-
-
-void main() {
-  runApp(Myapp());
+void main(){
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: HomePage(),
     );
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  State<Home> createState() => _HomeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeState extends State<Home> {
+class _HomePageState extends State<HomePage> {
+  bool _value = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("My app"),
-        ),
-        body:DismissibleExample(),
-        );
-  }
-}
-
-class DismissibleExample extends StatefulWidget {
-   DismissibleExample({super.key});
-
-  @override
-  State<DismissibleExample> createState() => _DismissibleExampleState();
-}
-
-class _DismissibleExampleState extends State<DismissibleExample> {
-List list = [
-  {'title' : 'ListTile 1'},
-  {'title' : 'ListTile 2'},
-  {'title' : 'ListTile 3'},
-  {'title' : 'ListTile 4'},
-  {'title' : 'ListTile 5'},
-  {'title' : 'ListTile 6'},
-  {'title' : 'ListTile 7'},
-  {'title' : 'ListTile 8'},
-  {'title' : 'ListTile 9'},
-  {'title' : 'ListTile 10'},
-  {'title' : 'ListTile 11'},
-  {'title' : 'ListTile 12'},
-  {'title' : 'ListTile 13'},
-  {'title' : 'ListTile 14'},
-  {'title' : 'ListTile 15'},
-];
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: list.length,
-      itemBuilder: (context , index){
-        return Dismissible(
-            onDismissed: (direction) => setState(() {
-              list.removeAt(index);
-            }),
-            background: Container(
-              color: Colors.green,
-              child: Icon(Icons.delete),
-            ),
-            key: ValueKey(list[index]),
-            
-          
-          
-          child: ListTile(
-            title: Text(list[index]['title']),
-            shape: Border.all(color: Colors.green),
-            
-          ));
-        
-      }
+      appBar: AppBar(
+        title: Text("JSON Data Fetching"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(_value == false? "Unchecked" : "Checked"),
+          Checkbox(value: _value, onChanged: (bool? val){
+            setState(() {
+              _value = val!;
+            });
+          })
+          ],
+        )
+      ),
       
-      );
+    );
   }
 }
--- lib/random.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:math';
-
-void main(){
-  var ran = Random();
-
- for (var i = 0; i < 5; i++) {
-   print(ran.nextInt(60));
- }
- }
